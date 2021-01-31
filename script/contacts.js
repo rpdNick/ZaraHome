e22f362d2bb5761f5805483a80c85dee12f30029
rpdNick <rpd_nick@ukr.net>
2021-01-31 23:23:59 +0200

    phone prefix, apdating contacts validation

--- script/contacts.js
+++ script/contacts.js
@@ -12,7 +12,6 @@ const form = document.getElementById('contacts-form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let el = document.querySelectorAll('[data-reqired]');
-    console.log(el)
     let erroreArrayElemnts = [];
     for (var i = 0; i < el.length; i++) {
         if (el[i].value === '') {
@@ -30,15 +29,21 @@ form.addEventListener('submit', (e) => {
         //     $('#phone').parents('.contact-box').find('.data-error').text('Пожалуйста, проверьте корректность заполнения поля "Телефон"');
         //     erroreArrayElemnts.push(el[i]);
         // }
-
-        if (!ValidateEmail($("#email").val())) {
-            erroreArrayElemnts.push('invalid_email');
+    }
+    if (!ValidateEmail($("#email").val())) {
+        erroreArrayElemnts.push('invalid_email');
+        if($("#email").val() === ''){
+            $('#email').parents('.contact-box').find('.invalid_email').text('* Поле обязательно к заполнению');
+            $('#email').parents('.contact-box').find('.invalid_email').show();
+        }else {
+            $('#email').parents('.contact-box').find('.invalid_email').text('* Неправильный электронный адрес');
             $('#email').parents('.contact-box').find('.invalid_email').show();
         }
     }
     if (erroreArrayElemnts.length == 0) {
         form.submit();
     }
+
 });
 function ValidateEmail(email) {
     var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
